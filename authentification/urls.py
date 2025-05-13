7f1b7190f227674e9647d9efb7a4e34469305684
Marissa <marissalamrii@gmail.com>
2025-05-13 17:23:46 +0200

    Sign on Page

--- authentification/urls.py
+++ authentification/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 import authentification.views
 
 urlpatterns = [
-    path('signup/', authentification.views.signup_page, name='signup')
+    path('signup/', authentification.views.signup_page, name='signup'),
+    path('signon/', authentification.views.login_page, name='signon'),
+
 ]
--- authentification/views.py
+++ authentification/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.shortcuts import redirect, render
 
 from . import forms
@@ -15,3 +15,23 @@ def signup_page(request):
             login(request, user)
             return redirect(settings.LOGIN_REDIRECT_URL)
     return render(request, 'signup.html', context={'form': form})
+
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    user = None
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+    if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+    if user is not None:
+                login(request, user)
+                message = f'Bonjour, {user.username}! Vous êtes connecté.'
+    else:
+                message = 'Identifiants invalides.'
+    return render(request, 'signon.html', context={'form': form, 'message': message})
+    
