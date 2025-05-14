1518be76ca2bf1848dd3539f7a4972e81514725f
Marissa <marissalamrii@gmail.com>
2025-05-14 16:51:31 +0200

    Comments add, and README.md file updated

--- authentification/forms.py
+++ authentification/forms.py
@@ -1,14 +1,15 @@
+#Création du fichier forms.py qui contiendra les formulaires (= convention)
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django import forms 
+from django import forms #Permet de créer des formulaire HTML en python
 
-
-class SignupForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+#Inscription
+class SignupForm(UserCreationForm): #On pars d'un formulaire d'inscription de django qui fournit deja des champs
+    class Meta(UserCreationForm.Meta): #Meta est une classe à l"intérieur d'"une classe, pas besoin d'en créer une autre car elle est deja dans le modele de django
         model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name', 'age', 'niveau_choix')
+        fields = ('username', 'email', 'first_name', 'last_name', 'age', 'niveau_choix') #Je veux que mes formulaires affiche ces champs-la
 
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50, label = "Username")
-    password = forms.CharField(max_length=50, widget=forms.PasswordInput, label = "Password")
+#Connexion, formulaire html en python = boite vide avec des champs
+class LoginForm(forms.Form): 
+    username = forms.CharField(max_length=50, label = "Username") #varchar, max 50, sur le site ce sera appelé Username
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput, label = "Password") #Widget détermine la façon dont on affiche le champ
--- authentification/views.py
+++ authentification/views.py
@@ -10,28 +10,34 @@ def signup_page(request):
     if request.method == 'POST':
         form = forms.SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save() #enregistre utilisateur en bdd et stock en variable
             # auto-login user
             login(request, user)
             return redirect(settings.LOGIN_REDIRECT_URL)
     return render(request, 'signup.html', context={'form': form})
 
 
-def login_page(request):
-    form = forms.LoginForm()
+def login_page(request): #Demande que je fais au serveur en cliquant = requete est un objet
+    form = forms.LoginForm() #On appelle la classe login Form 
     message = ''
     user = None
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
-    if form.is_valid():
-            user = authenticate(
+   # print(request.method)
+    if request.method == 'POST': #Si je poste des données
+        form = forms.LoginForm(request.POST) #on rentre les données dans le formulaire
+       # print(request.POST)
+    #print(form.is_valid())
+    if form.is_valid(): #Si tout les champs on été rentré et qu'il respecte les conditions (max50 caracteres...)
+            user = authenticate( #Cette méthode elle va servir à vérifier que la personne existe dèja dans la BDD
                 username=form.cleaned_data['username'],
                 password=form.cleaned_data['password'],
             )
-    if user is not None:
+            #print(user) = Si la personne existe dans la BDD on verra son nom, sinon c'est none
+            
+    if user is not None: #Dans le cas ou on le trouve dans la BDD
                 login(request, user)
                 message = f'Bonjour, {user.username}! Vous êtes connecté.'
+                
     else:
                 message = 'Identifiants invalides.'
-    return render(request, 'signon.html', context={'form': form, 'message': message})
+    return render(request, 'signon.html', context={'form': form, 'message': message}) #Le render permet d'afficher la page html
     
