acb0f04013b769990faebb6ece386ff479ea277a
Marissa <marissalamrii@gmail.com>
2025-05-27 18:36:43 +0200

    New fonctionality : UPDATE

--- recettes/urls.py
+++ recettes/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('homepage/',views.home_page, name='home_page'),
     path('create_recipes/', views.create_recipes, name = 'create_recipes'),
     path('recipes_details/<id>/', views.recipe_details, name="recipes_details"),
-    #path('recipe_details/<id>/recipe_update', views.recipe_update, name='recipe_update'),
+    path('update_recipes/<id>/', views.recipe_update, name='update_recipes'),
     path('my_recipes/', views.my_recipes, name="my_recipes")
 
 
--- recettes/views.py
+++ recettes/views.py
@@ -7,9 +7,8 @@ from .models import Recettes
 @login_required #Peut pas y accèder si t'es pas connecter 
 def home_page(request):
      all_recette= Recettes.objects.all()
-     print(all_recette)
      context = {
-        'recettes' : all_recette 
+        'recettes' : all_recette,
      }
      return render(request, 'homepage.html', context=context )
 
@@ -32,15 +31,25 @@ def recipe_details(request, id):
      recette = get_object_or_404(Recettes, id=id)
      return render(request, 'recipes_details.html', {'recette': recette})
 
+@login_required
 def my_recipes(request):
-     print(request.user)
      mes_recettes = Recettes.objects.filter(user=request.user)
-     return render(request, 'my_recipes.html', {'mes_recettes': mes_recettes})
+     return render(request, 'homepage.html', {'recettes': mes_recettes, 'source': 'my_recipes'})
 
-#def recipe_update(request, id):
-     #recette = Recettes.objects.get(id=id)
-     #form = forms.Creation(instance=Recettes)
-     #return render (request,)
+@login_required
+def recipe_update(request, id):
+     recette = Recettes.objects.get(id=id)
+     if request.method == "GET":
+          form = forms.Creation(instance=recette)
+          return render(request, 'recipes_creation.html', {'form': form} )
+     if request.method == "POST":
+          form = forms.Creation(request.POST, instance=recette)
+          if form.is_valid():
+              recipe = form.save()
+          return redirect('home_page')
+     
+     
+     
 
 
 
